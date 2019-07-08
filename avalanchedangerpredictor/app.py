ad4fd7e1ffe648920e72bb76f82bf3c3259ad14d
Kevin <kevronv@uw.edu>
2019-07-08 13:50:30 -0700

    pickle new model

--- avalanchedangerpredictor/app.py
+++ avalanchedangerpredictor/app.py
@@ -62,7 +62,7 @@ def predict():
     input_1day_max_temp = float(input_1day_max_temp_text)
 
     input_1day_min_temp_text = data['input_1day_min_temp_09']
-    input_1day_min_temp = float(input_1day_min_temp)
+    input_1day_min_temp = float(input_1day_min_temp_text)
     
     input_2day_max_temp_text = data['input_2day_max_temp_10']
     input_2day_max_temp = float(input_2day_max_temp_text)
@@ -71,31 +71,31 @@ def predict():
     input_2_day_min_temp = float(input_2_day_min_temp_text)
     
     input_1day_max_snow_text = data['input_1day_max_snow_12']
-    input_max_1_day = float(input_1day_max_snow_text)
+    input_max_1_day_snow = float(input_1day_max_snow_text)
     
     input_2day_max_snow_text = data['input_2day_max_snow_13']
     input_2day_max_snow = float(input_2day_max_snow_text)
     
-    input_3day_max_snow_14_text = data['input_3day_max_snow_14']
-    input_3day_max_snow_14 = float(input_3day_max_snow_14_text)
+    input_3day_max_snow_text = data['input_3day_max_snow_14']
+    input_3day_max_snow = float(input_3day_max_snow_text)
     
     input_precip_text = data['input_precip_15']
     input_precip = float(input_precip_text)
     
-    
-    arguments = pd.DataFrame(
-        [[solar_input, temp_input,input_min_wind_speed,
-        input_wind_direction,input_total_snow_depth, input_max_1_day_temp,
-        input_max_2_day_temp, input_max_1_day_snow, input_max_2_day_snow,
-        input_max_3_day_snow, input_precipitation]],
-        columns = ['Battery Voltage (v)', 'Temperature (deg F)','Wind Speed Minimum (mph)', 
-         'Wind Speed Average (mph)','Wind Speed Maximum (mph)',
-         'Wind Direction (deg.)','24 Hour Snow (in)', 'Total Snow Depth (in)',
-         'max_1_day_temp','min_1_day_temp', 'max_2_day_temp',
-         'min_2_day_temp','max_1_day_snow','max_2_day_snow', 
-         'max_3_day_snow', '4800_brooks']
-    )
-    # prediction = model.predict_proba([data['user_input']])
+    arguments = pd.DataFrame([[
+        input_solar, input_temp, input_wind_speed_min, input_wind_speed_avg, input_wind_speed_max,
+        input_wind_direction, input_snowpack_height,
+        input_1day_max_temp, input_1day_min_temp,
+        input_2day_max_temp, input_2_day_min_temp,
+        input_max_1_day_snow, input_2day_max_snow,
+        input_3day_max_snow, input_precip]],
+        columns = ['Battery Voltage (v)', 'Temperature (deg F)',
+        'Wind Speed Minimum (mph)', 'Wind Speed Average (mph)',
+        'Wind Speed Maximum (mph)', 'Wind Direction (deg.)',
+        '  Total Snow Depth (in)', 'max_1_day_temp', 'min_1_day_temp',
+        'max_2_day_temp', 'min_2_day_temp', 'max_1_day_snow', 'max_2_day_snow',
+        'max_3_day_snow', '4800_brooks'])
+    print(arguments.head(1))
     
     predicted_score = model.predict(arguments)
     rounded_predicted_score = round(predicted_score)
